cb6dad004cd74f7a524c2df3bca0c610611bacf8
KaKeGo <funfcia@hotmail.com>
2023-09-26 22:42:54 +0200

    Custom footer

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook, faInstagram, faTiktok 
+} from '@fortawesome/free-brands-svg-icons';
+
+import './Footer.css'
+
 const Footer = () => {
+  const currentDate = new Date();
+  const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const formattedDate = currentDate.toLocaleDateString('en-GB', dateOptions);
+
   return (
-    <footer className='mx-3'>
-      <p className='font'>&copy; Created by Kakego</p>
-    </footer>
-  )
-}
+      <>
+      <div className='footer__brake'></div>
+      <footer className='footer p-4'>
+          <div className='footer-content'>
+              <div className='footer-left'>
+                  <p className='font'>&copy; Created by Kakego</p>
+                  <p>created: 20/06/2023 - {formattedDate}</p>
+              </div>
+              <div className='footer-right'>
+              <ul className='social-icons'>
+                  <li><a href='#'><FontAwesomeIcon icon={faFacebook} /></a></li>
+                  <li><a href='#'><FontAwesomeIcon icon={faInstagram} /></a></li>
+                  <li><a href='#'><FontAwesomeIcon icon={faTiktok} /></a></li>
+              </ul>
+              </div>
+          </div>
+      </footer>
+      </>
+  );
+};
 
 export default Footer
