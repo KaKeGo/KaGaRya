6b69c090cc0c8d1176d5e37c8facbcf6e1b49576
KaKeGo <funfcia@hotmail.com>
2023-10-16 21:34:53 +0200

    Create message pages for activated account

--- src/containers/Pages/AccountActivated/AccountActivatedError/AccountActivatedError.jsx
+++ src/containers/Pages/AccountActivated/AccountActivatedError/AccountActivatedError.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import './AccountActivatedError.css'
+
 const AccountActivatedError = () => {
   return (
-    <div>AccountActivatedError</div>
+    <div className='mess__error__container'>
+      <div className='mess__error__content'>
+
+        <div className='mess__error__title'>
+          <h2>Account activated error</h2>
+        </div>
+
+        <div className='mess__error__body'>
+          <p>
+            Something went wrong with activated your account. Please contact with
+            support. We try to help you.
+          </p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
