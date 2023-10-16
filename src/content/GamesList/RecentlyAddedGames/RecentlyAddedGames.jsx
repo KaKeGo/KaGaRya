3eac63be7443aea55f275ca59d7d308184652cdb
KaKeGo <funfcia@hotmail.com>
2023-10-17 00:03:28 +0200

    add style for last added games boxes

--- src/content/GamesList/RecentlyAddedGames/RecentlyAddedGames.jsx
+++ src/content/GamesList/RecentlyAddedGames/RecentlyAddedGames.jsx
@@ -14,19 +14,39 @@ const RecentlyAddedGames = () => {
 
             <div className='recently__games__container'>
                 <div className='recently__games__box'>
-                    <div className='recently__games__content'></div>
+                    <div className='recently__games__content'>
+                        <img className='game__icon'
+                            src='https://c4.wallpaperflare.com/wallpaper/393/143/799/assassins-creed-the-ezio-collection-4k-wallpaper-preview.jpg'/>
+                        <div className='game__title'>Assassin Creed 2</div>
+                    </div>
                 </div>
                 <div className='recently__games__box'>
-                    <div className='recently__games__content'></div>
+                    <div className='recently__games__content'>
+                    <img className='game__icon'
+                            src='https://images8.alphacoders.com/414/414551.jpg'/>
+                            <div className='game__title'>Splinter Cell: Black List</div>
+                    </div>
                 </div>
                 <div className='recently__games__box'>
-                    <div className='recently__games__content'></div>
+                    <div className='recently__games__content'>
+                        <img className='game__icon'
+                            src='https://wallpapers.com/images/featured-full/best-gaming-background-n1s8zlfrny49wo0q.jpg'/>
+                        <div className='game__title'>Wolvenstein: New order</div>
+                    </div>
                 </div>
                 <div className='recently__games__box'>
-                    <div className='recently__games__content'></div>
+                <div className='recently__games__content'>
+                    <img className='game__icon'
+                        src='https://getwallpapers.com/wallpaper/full/a/8/f/33274.jpg'/>
+                        <div className='game__title'>Mortal Combat 11</div>
+                    </div>
                 </div>
                 <div className='recently__games__box'>
-                    <div className='recently__games__content'></div>
+                <div className='recently__games__content'>
+                    <img className='game__icon'
+                        src='https://getwallpapers.com/wallpaper/full/a/7/f/33098.jpg'/>
+                    <div className='game__title'>Hitman 4</div>
+                    </div>
                 </div>
             </div>
 
