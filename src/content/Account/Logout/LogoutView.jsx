8566363fff2306253deeafe4186a8228996448f5
KaKeGo <funfcia@hotmail.com>
2023-10-22 02:40:03 +0200

    fix

--- src/content/Account/Logout/LogoutView.jsx
+++ src/content/Account/Logout/LogoutView.jsx
@@ -10,7 +10,7 @@ const LogoutView = ({ children }) => {
 
     const handleLogout = async () => {
         await dispatch(Logout())
-        window.location.reload()
+        // window.location.reload()
     }
 
     return (
