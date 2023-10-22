63063b0db645e7ce8ea072c3c24b60bbaad6300a
KaKeGo <funfcia@hotmail.com>
2023-10-22 11:50:23 +0200

    fix

--- src/content/Account/Logout/LogoutView.jsx
+++ src/content/Account/Logout/LogoutView.jsx
@@ -10,6 +10,13 @@ const LogoutView = ({ children }) => {
 
     const handleLogout = async () => {
         await dispatch(Logout())
+
+        let allCookies = document.cookie.split(';');
+
+        for (let i = 0; i < allCookies.length; i++) {
+            document.cookie = allCookies[i] + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        }
+
         // window.location.reload()
     }
 
