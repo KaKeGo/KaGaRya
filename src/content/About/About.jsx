7368fc550329ba337c0cae0f84ae757693bb5fe2
Hermi-San <mati.labenda@gmail.com>
2023-10-23 19:20:39 +0200

    About section created

--- src/content/About/About.jsx
+++ src/content/About/About.jsx
@@ -1,30 +1,62 @@
-import React from 'react'
+import React from "react";
 
-import './About.css'
+import "./About.css";
 
 const About = () => {
-  return (
-  
+	return (
+		<div className="about__page containers">
+			<div className="about__box">
+				<div className="about__us__title">
+					<h2>About Us</h2>
+				</div>
 
-  <div className='about__page containers' >
-    <div className='about__box'>
+				<div className="about__us__content">
+					<p>
+						So here will be something about us but I have no idea what to write
+						here right now :3{""}
+					</p>
+				</div>
 
-      <div className='about__title'>
-        <h2>About Us</h2>
-        
-      </div>
+				<div className="about__plans__title">
+					<h2>What we are planning to do</h2>
+				</div>
 
+				<div className="about__plans__content">
+					<p>
+						We are planning to make fully free site on which you will be able to
+						find everything starting from food to video games which users will
+						post by themselfs and they will be able to vote on particular things
+						as in "How good is it"
+					</p>
+				</div>
 
+				<div className="about__team__title">
+					<h2>Members of KaGaRya project</h2>
+				</div>
 
+				<div className="about__team__content">
+					<p>
+						Main developer "Kakego" Frontend-Backend <br />
+						Developer "Activ" Frontend <br />
+						Developer "Hermi" Frontend
+					</p>
+				</div>
 
-      <div className='about__content'>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta blanditiis vel aspernatur fuga accusantium deleniti asperiores non architecto suscipit delectus consequatur optio eaque sequi minus enim hic, unde quam rerum tempore, aperiam saepe, doloremque fugit. Natus officia assumenda, beatae cupiditate dolorem atque magni nobis provident itaque voluptas, quod soluta? Deserunt provident doloremque nam officia, accusamus eum earum incidunt error quas animi asperiores! Ipsam corrupti impedit aliquam a, debitis iusto nesciunt obcaecati non nemo commodi, odio, quae ducimus tempore accusamus fugiat error libero quam necessitatibus. Ab molestias nemo libero corporis, culpa nulla doloribus. Harum assumenda aliquid eius dolorum, adipisci expedita eligendi.</p>
-      </div>
-    </div>
-  </div>
-    
+				<div className="about__why__title">
+					<h2>Why was KaGaRya made</h2>
+				</div>
 
-  )
-}
+				<div className="about__why__content">
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
+						sequi nam placeat, doloribus officiis praesentium unde consequatur
+						explicabo, quos incidunt eveniet, officia tempora autem in
+						repudiandae esse eaque veniam alias.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default About
+export default About;
