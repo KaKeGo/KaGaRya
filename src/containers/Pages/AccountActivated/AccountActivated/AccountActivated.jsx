d7ff6f12d7e5442d3714fa3c87f60318a89335fc
Romaffaelito <driszke@o2.pl>
2023-10-29 19:38:11 +0100

    ani

--- src/containers/Pages/AccountActivated/AccountActivated/AccountActivated.jsx
+++ src/containers/Pages/AccountActivated/AccountActivated/AccountActivated.jsx
@@ -1,28 +1,51 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamation } from '@fortawesome/free-solid-svg-icons'
+
 import './AccountActivated.css'
 
 const AccountActivated = () => {
   return (
-    <div className='mess__info__container'>
-      <div className='mess__info__content'>
+    <div className="r_a">
+    <div className="reg_act">
+       <p><FontAwesomeIcon icon={faExclamation}>!</FontAwesomeIcon></p>
+      <div className="reg_act_circle">
+      <h2 className="reg_act_text"> 
+      <span style={{transform: 'rotate(0deg)'}}>a</span>
+      <span style={{transform: 'rotate(13.8deg)'}}>c</span>
+      <span style={{transform: 'rotate(27.6deg)'}}>c</span>
+      <span style={{transform: 'rotate(41.4deg)'}}>o</span>
+      <span style={{transform: 'rotate(55.2deg)'}}>u</span>
+      <span style={{transform: 'rotate(69deg)'}}>n</span>
+      <span style={{transform: 'rotate(82.8deg)'}}>t</span>
+      <span style={{transform: 'rotate(96.6deg)'}}></span>
+      <span style={{transform: 'rotate(110.4deg)'}}>a</span>
+      <span style={{transform: 'rotate(124.2deg)'}}>l</span>
+      <span style={{transform: 'rotate(138deg)'}}>r</span>
+      <span style={{transform: 'rotate(151.8deg)'}}>e</span>
+      <span style={{transform: 'rotate(165.6deg)'}}>a</span>
+      <span style={{transform: 'rotate(179.4deg)'}}>d</span>
+      <span style={{transform: 'rotate(193.2deg)'}}>y</span>
+      <span style={{transform: 'rotate(207deg)'}}> </span>
+      <span style={{transform: 'rotate(220.8deg)'}}>a</span>
+      <span style={{transform: 'rotate(234.6deg)'}}>c</span>
+      <span style={{transform: 'rotate(248.4deg)'}}>t</span>
+      <span style={{transform: 'rotate(262.2deg)'}}>i</span>
+      <span style={{transform: 'rotate(276deg)'}}>v</span>
+      <span style={{transform: 'rotate(289.8deg)'}}>a</span>
+      <span style={{transform: 'rotate(303.6deg)'}}>t</span>
+      <span style={{transform: 'rotate(317.4deg)'}}>e</span>
+      <span style={{transform: 'rotate(331.2deg)'}}>d</span>
+      <span style={{transform: 'rotate(345deg)'}}> </span>
 
-        <div className='mess__info__title'>
-          <h2>Account already activated</h2>
-        </div>
-
-        <div className='mess__info__body'>
-          <p>
-            Account was activated. Try to 
-              <a className='link'
-                href='http://localhost:3000/login'> login </a> 
-              now.
-          </p>
-        </div>
-
+      </h2>
+      </div>
       </div>
     </div>
   )
 }
 
 export default AccountActivated
+
+/* zółte */
--- src/containers/Pages/AccountActivated/AccountActivatedSuccess/AccountActivatedSuccess.jsx
+++ src/containers/Pages/AccountActivated/AccountActivatedSuccess/AccountActivatedSuccess.jsx
@@ -1,31 +1,55 @@
 import React from 'react'
-
-
 import './AccountActivatedSuccess.css'
+import {Link} from 'react-router-dom'
 
 
 const AccountActivatedSuccess = () => {
-
-    return (
-    <div className='mess__success__container'>
-      <div className='mess__success__content'>
-
-        <div className='mess__success__title'>
-          <h2>Account activated</h2>
-        </div>
-
-        <div className='mess__success__body'>
-          <p>
-            Account was activated. Try to 
-              <a className='link'
-                href='http://localhost:3000/login'> login </a> 
-              now.
-          </p>
-        </div>
-
+  return (
+    <div className="r_s">
+    <div className="reg_suc">
+       <p>&#10003;</p>
+      <div className="reg_suc_circle">
+      <h2 className="reg_suc_text"> 
+      <span style={{transform: 'rotate(0deg)'}}>a</span>
+      <span style={{transform: 'rotate(10.5deg)'}}>c</span>
+      <span style={{transform: 'rotate(21deg)'}}>c</span>
+      <span style={{transform: 'rotate(31.5deg)'}}>o</span>
+      <span style={{transform: 'rotate(42deg)'}}>u</span>
+      <span style={{transform: 'rotate(52.5deg)'}}>n</span>
+      <span style={{transform: 'rotate(63deg)'}}>t</span>
+      <span style={{transform: 'rotate(73.5deg)'}}></span>
+      <span style={{transform: 'rotate(84deg)'}}>a</span>
+      <span style={{transform: 'rotate(94.5deg)'}}>c</span>
+      <span style={{transform: 'rotate(105.5deg)'}}>t</span>
+      <span style={{transform: 'rotate(116deg)'}}>i</span>
+      <span style={{transform: 'rotate(125deg)'}}>v</span>
+      <span style={{transform: 'rotate(136.5deg)'}}>a</span>
+      <span style={{transform: 'rotate(147deg)'}}>t</span>
+      <span style={{transform: 'rotate(157.5deg)'}}>e</span>
+      <span style={{transform: 'rotate(168deg)'}}>d</span>
+      <span style={{transform: 'rotate(178.5deg)'}}> </span>
+      <span style={{transform: 'rotate(189deg)'}}>-</span>
+      <span style={{transform: 'rotate(199.5deg)'}}> </span>
+      <Link to='/login'><span style={{transform: 'rotate(210deg)'}}>g</span>
+      <span style={{transform: 'rotate(220.5deg)'}}>o</span>
+      <span style={{transform: 'rotate(231deg)'}}> </span>
+      <span style={{transform: 'rotate(241.5deg)'}}>t</span>
+      <span style={{transform: 'rotate(252deg)'}}>o</span>
+      <span style={{transform: 'rotate(262.5deg)'}}> </span>
+      <span style={{transform: 'rotate(273deg)'}}>l</span>
+      <span style={{transform: 'rotate(283.5deg)'}}>o</span>
+      <span style={{transform: 'rotate(295deg)'}}>g</span>
+      <span style={{transform: 'rotate(307deg)'}}>i</span>
+      <span style={{transform: 'rotate(315deg)'}}>n</span>
+      <span style={{transform: 'rotate(325.5deg)'}}> </span>
+      <span style={{transform: 'rotate(339deg)'}}>-</span></Link>
+      <span style={{transform: 'rotate(346.5deg)'}}> </span>
+      </h2>
+      </div>
       </div>
     </div>
-    )
+  )
 }
+
 
 export default AccountActivatedSuccess
