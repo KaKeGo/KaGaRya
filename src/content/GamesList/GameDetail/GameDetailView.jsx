e39a22de23d1418d25288281968422b15b3dfcff
KaKeGo <funfcia@hotmail.com>
2023-10-29 21:05:29 +0100

    f

--- src/content/GamesList/GameDetail/GameDetailView.jsx
+++ src/content/GamesList/GameDetail/GameDetailView.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import CommingSoon from '../../../containers/CommingSoon/CommingSoon'
+
 import './GameDetailView.css'
 
 const GameDetailView = () => {
   return (
     <div className='gamedetail__container containers'>
         <div className='gamedetail__box b'>
+          <CommingSoon />
 
             <div className='gamedetail__column1'>
                 <div className='gamedetail__cover'>
